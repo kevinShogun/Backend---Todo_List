61ea73993be6cbb2c2c61a838945aac06b6b8570
Kevin Garcia <54457637+kevinShogun@users.noreply.github.com>
2021-02-10 13:40:32 -0600

    Se arreglo el Bug de Cache

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,59 +1,58 @@
-const Usuario = require("../models/Usuario");
-const bcryptjs = require("bcryptjs");
-const { validationResult } = require("express-validator");
-const jwt = require("jsonwebtoken");
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.crearUsuario = async (req, res) => {
-	// revisar si hay errores
-	const errores = validationResult(req);
-	if (!errores.isEmpty()) {
-		return res.status(400).json({ errores: errores.array() });
-	}
 
-	// extraer email y password
-	const { email, password } = req.body;
+    // revisar si hay errores
+    const errores = validationResult(req);
+    if( !errores.isEmpty() ) {
+        return res.status(400).json({errores: errores.array() })
+    }
 
-	try {
-		// Revisar que el usuario registrado sea unico
-		let usuario = await Usuario.findOne({ email });
+    // extraer email y password
+    const { email, password } = req.body;
 
-		if (usuario) {
-			return res.status(400).json({ msg: "El usuario ya existe" });
-		}
 
-		// crea el nuevo usuario
-		usuario = new Usuario(req.body);
+    try {
+        // Revisar que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({ email });
 
-		// Hashear el password
-		const salt = await bcryptjs.genSalt(10);
-		usuario.password = await bcryptjs.hash(password, salt);
+        if(usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe' });
+        }
 
-		// guardar usuario
-		await usuario.save();
+        // crea el nuevo usuario
+        usuario = new Usuario(req.body);
 
-		// Crear y firmar el JWT
-		const payload = {
-			usuario: {
-				id: usuario.id,
-			},
-		};
+        // Hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt );
 
-		// firmar el JWT
-		jwt.sign(
-			payload,
-			process.env.SECRETA,
-			{
-				expiresIn: 96000, // 1 hora
-			},
-			(error, token) => {
-				if (error) throw error;
+        // guardar usuario
+        await usuario.save();
 
-				// Mensaje de confirmación
-				res.json({ token });
-			}
-		);
-	} catch (error) {
-		console.log(error);
-		res.status(400).send(" ");
-	}
-};
+        // Crear y firmar el JWT
+        const payload = {
+            usuario: {
+                id: usuario.id
+            }
+        };
+
+        // firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1 hora
+        }, (error, token) => {
+            if(error) throw error;
+
+            // Mensaje de confirmación
+            res.json({ token  });
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
+}
