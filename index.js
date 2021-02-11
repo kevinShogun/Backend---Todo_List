7d4ead1233f575d6d2bd866f47a8eecad2324f10
Kevin Garcia <54457637+kevinShogun@users.noreply.github.com>
2021-02-10 18:05:55 -0600

    Ajustes para deployment en variables

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // Importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -22,6 +22,6 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/todolists", require("./routes/todoLists"));
 app.use("/api/tasks", require("./routes/tasks"));
 
-app.listen(port, '0.0.0.0', () => {
-	console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
